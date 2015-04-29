27050922103b574712ede8cba08809a537c37b76
benbuchar <bbuchar@oswego.edu>
2015-04-29 16:39:35 -0400

    Added XML Format

--- Grails-Site/grails-app/domain/helloworld/Game.groovy
+++ Grails-Site/grails-app/domain/helloworld/Game.groovy
@@ -2,7 +2,7 @@
 package helloworld
 import grails.rest.*
 
-@Resource(uri='/games', formats=['json'])
+@Resource(uri='/games', formats=['json', 'xml'])
 class Game {
 	String title
 	String developer
